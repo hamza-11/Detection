4549a7cde84a95e6ed917ce93442326e3c91f69e
hamza-11 <59791027+hamza-11@users.noreply.github.com>
2024-10-29 20:33:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -120,4 +120,4 @@ def get_detections():
     return jsonify(detected_objects)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5100, debug=True)
